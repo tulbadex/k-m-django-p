81e0d495a45951cfd7ff9415d69d3fa8e2e340e8
Ibrahim Adedayo <ibrahimadedayo@rocketmail.com>
2024-05-07 02:44:13 -0700

    added statis file root

--- webapps2024/settings.py
+++ webapps2024/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -125,7 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS =(os.path.join(BASE_DIR, 'static'),)
+# STATICFILES_DIRS =(os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"), # Django will search for /src/
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
