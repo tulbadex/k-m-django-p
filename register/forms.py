fbd0783d80b113469da22ea0e20aa9ce0b2e4884
Ibrahim Adedayo <ibrahimadedayo@rocketmail.com>
2024-05-10 02:48:39 -0700

    added messages to custom user creation form

--- register/forms.py
+++ register/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
+from django.core.validators import EmailValidator
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -9,6 +10,24 @@ class CustomUserCreationForm(UserCreationForm):
         ('USD', 'US Dollars'),
         ('EUR', 'Euros'),
     ]
+    username = forms.CharField(max_length=150, required=True, help_text='Required. Unique.')
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+    email = forms.CharField(max_length=150, required=True, validators=[EmailValidator], help_text='Must be a valid email address.')
+    password1 = forms.CharField(
+        label="Password",
+        strip=False,
+        widget=forms.PasswordInput,
+        help_text="Your password must contain at least 8 characters.",
+        required=True
+    )
+    password2 = forms.CharField(
+        label="Password confirmation",
+        widget=forms.PasswordInput,
+        strip=False,
+        help_text="Enter the same password as before, for verification.",
+        required=True,
+    )
     currency = forms.ChoiceField(choices=CURRENCY_CHOICES)
 
     class Meta(UserCreationForm.Meta):
