f9386fb31e50c49411ee1d9da6d5ac85f4e3f9bc
Ibrahim Adedayo <ibrahimadedayo@rocketmail.com>
2024-05-10 02:55:46 -0700

    added messages to custom user creation form

--- webapps2024/settings.py
+++ webapps2024/settings.py
@@ -24,7 +24,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = 'django-insecure-^cuq4448!kqkx)st_g_$^y3x6svdj++az2l1z2gxgqvyaomet4'
 
 # SECURITY WARNING: don't run with debug turned on in production! False
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
